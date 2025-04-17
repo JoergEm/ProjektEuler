ec0b36ca0cf6a4a278d5f63c289d437d643fb6b0
Jörg Mekka <elvishasleftyahoo@googlemail.com>
2025-04-17 13:29:57 +0200

    0001multiples_of_3_or_5.kt Init

---
+++ Kotlin/0001multiples_of_3_or_5.kt
@@ -0,0 +1,15 @@
+/*
+ * ProjectEuler.kt
+ *
+ * Copyright 2025 @GitHub JoergEm
+ *
+ * This program is WITHOUT ANY WARRANTY; without even the implied
+ * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ *
+ *
+ */
+
+ fun main() {
+	 val sum = (1..999).filter{it%3 == 0||it%5 ==0}.sum()
+	 println(sum)
+ }
