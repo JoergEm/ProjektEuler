70fb4f117fce3236881817b208e0aad4d54dfe11
Jörg Mekka <elvishasleftyahoo@googlemail.com>
2025-05-06 18:33:26 +0200

    0001multiples_of_3_or_5.fs Init

---
+++ F#/0001multiples_of_3_or_5.fs
@@ -0,0 +1,14 @@
+
+﻿(*
+.NET VERSION 7.0.410
+0001multiples_of_3_or_5.fs
+Copyright 2025 @GitHub JoergEm
+This program is WITHOUT ANY WARRANTY; without even the implied
+warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+*)
+
+let summe =
+    [1 .. 999]
+    |> List.filter (fun nummer -> nummer % 3 = 0 || nummer % 5 = 0)
+    |> List.sum
+printfn "%d" summe
