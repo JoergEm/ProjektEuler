9ee7406013bd1a0390409c0c3470cefdbc238ef3
Jörg Mekka <elvishasleftyahoo@googlemail.com>
2025-05-08 21:07:26 +0200

    0001multiples_of_3_or_5.m Init

---
+++ Octave/0001multiples_of_3_or_5.m
@@ -0,0 +1,18 @@
+%{
+% octave version 8.4.0
+% 0001multiples_of_3_or_5.m
+% Copyright 2025 @GitHub
+%
+% This program is WITHOUT ANY WARRANTY; without even the implied
+% warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+%}
+
+
+summe = 0;
+for nummer = 1:999
+    if mod(nummer, 3) == 0 || mod(nummer, 5) == 0
+        summe = summe + nummer;
+    end
+end
+
+fprintf('%d\n', summe);
