a9057d275f5cae422c311cbbecadb9abff7bf628
Jörg Mekka <elvishasleftyahoo@googlemail.com>
2025-05-12 16:45:16 +0200

    0001multiples_of_3_or_5.ml Init

---
+++ OCaml/0001multiples_of_3_or_5.ml
@@ -0,0 +1,13 @@
+(**
+OCaml toplevel 5.3.0
+0001multiples_of_3_or_5.ml
+Copyright 2025 @GitHub JoergEm
+This program is WITHOUT ANY WARRANTY; without even the implied
+warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+*)
+
+let () =
+  let numbers = List.init 1000 (fun x -> x) in
+  let filtered = List.filter (fun x -> x mod 3 = 0 || x mod 5 = 0) numbers in
+  let total = List.fold_left (+) 0 filtered in
+  Printf.printf "%d\n" total
